a618ef3a5a77b874cde3f1912fdaa0da9f311536
ZaharenkovAleksey <157736559+ZaharenkovAleksey@users.noreply.github.com>
2024-04-24 08:05:28 +0600

    Calculator

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
 
-
 function Header() {
   return (
     <div className='Header'>
-        <Link className="HeaderLink" to="/">Список калькуляторов</Link>
+        <Link className="HeaderLink" to="/">Кредитные калькуляторы</Link>
+        <Link className="HeaderLink" to="/Admin">Админ-панель</Link>
     </div>
   );
 }
